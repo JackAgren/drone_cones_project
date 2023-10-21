7ff81f4c1a650dfaa9314964032927117da00cee
Emma Lynn <emmaruthlynn@gmail.com>
2023-10-21 16:21:06 -0600

    order history, working on menu

---
+++ frontend/src/components/page-content/CYOMenu.vue
@@ -0,0 +1,127 @@
+<template>
+
+  <table style="width:100%;">
+    <tr>
+      <td>
+
+        <div class="small-center">
+          <h3>Number of Scoops</h3>
+          <p class="scoop-count"><span @click="decreaseScoops">&#x25C0;</span> {{scoopCount}} <span @click="increaseScoops">&#x25B6;</span></p>
+
+          <h3>Flavor(s)</h3>
+          <p class="descript">&#x25C0; Strawberry &#x25B6;</p>
+          <p class="descript">&#x25C0; Strawberry &#x25B6;</p>
+        </div>
+      </td>
+      <td>
+        <div class="small-center">
+          <h3>Cone</h3>
+          <p class="descript">&#x25C0; Waffle &#x25B6;</p>
+        </div>
+        <div class="small-center">
+          <h3>Toppings</h3>
+          <p class="descript">&#x25C0; Cookie Dough &#x25B6;</p>
+        </div>
+        <VueButton @click="addToCart" class="add-to-cart">
+          Add to Cart
+          <img class="cart-icon" src="../../assets/img/shopping-cart.png" alt="Shopping cart icon.">
+        </VueButton>
+
+      </td>
+    </tr>
+  </table>
+
+</template>
+
+<script>
+import Background from "@/components/Background.vue";
+import VueButton from "@/components/Button.vue";
+
+const MIN_SCOOPS = 1;
+const MAX_SCOOPS = 4;
+
+export default {
+  name: 'Menu',
+  components: {
+    Background,
+    VueButton,
+  },
+  data() {
+    return {
+      scoopCount: 2,
+    }
+  },
+  methods: {
+    addToCart() {
+      //TODO: add to cart
+    },
+    increaseScoops() {
+      if (this.scoopCount < MAX_SCOOPS) {
+        this.scoopCount++;
+      }
+    },
+    decreaseScoops() {
+      if (this.scoopCount > MIN_SCOOPS) {
+        this.scoopCount--;
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+.scoop-count {
+  text-align: center;
+  font-size: 30pt;
+  cursor: pointer;
+}
+
+.descript {
+  font-weight: normal;
+  text-align: center;
+}
+
+.add-to-cart {
+  position: absolute;
+  left: 60%;
+  top: 85%;
+}
+
+h3 {
+  font-size: 12pt;
+  text-align: center;
+  margin-top: 2mm;
+}
+
+.small-center {
+  padding: 10px 10px 0 10px;
+  margin: 10px;
+  color: rgba(40, 40, 40);
+  font-weight: bold;
+  text-align: left;
+  background-color: rgba(244, 255, 256, .95);
+  position: relative;
+}
+
+.cart-icon {
+  width: 20%;
+}
+
+input {
+  width: 70%;
+  height: 6%;
+  display: inline-block;
+}
+
+hr {
+  border: 2px solid black;
+}
+
+h2 {
+  margin-top: 3%;
+  font-size: 18pt;
+}
+
+
+</style>
--- frontend/src/components/pages/customer/Menu.vue
+++ frontend/src/components/pages/customer/Menu.vue
@@ -19,60 +19,11 @@
       <tr>
 
         <td>
-          <table>
-            <tr v-for="row in featured" style="height: 40%;">
-
-              <td v-for="item in row" style="width: 50%;">
-                <div class="small-center">
-
-                  <h3>{{item.name}}</h3>
-                  <img :class="item.css" :src="item.image" alt="Photo of featured cone.">
-
-                  <div>
-                    <p class="featured-price">${{ Math.round(item.price) / 100 }}</p>
-                    <div @click="addToCart" class="this-button">
-                      &nbsp;<img class="cart-icon" src="../../../assets/img/shopping-cart.png" alt="Shopping cart icon.">
-                    </div>
-                  </div>
-
-                </div>
-              </td>
-
-            </tr>
-          </table>
+          <FeaturedMenu/>
         </td>
 
         <td>
-          <table style="width:100%;">
-            <tr>
-              <td>
-
-                <div class="small-center">
-                  <h3>Number of Scoops</h3>
-                  <p style="text-align: center">&#x25C0; 2 &#9658;</p>
-
-                  <h3>Flavor(s)</h3>
-                  <p class="descript">&#x25C0; Strawberry &#9658;</p>
-                  <p class="descript">&#x25C0; Strawberry &#9658;</p>
-                </div>
-              </td>
-              <td>
-                <div class="small-center">
-                  <h3>Cone</h3>
-                  <p class="descript">&#x25C0; Waffle &#9658;</p>
-                </div>
-                <div class="small-center">
-                  <h3>Toppings</h3>
-                  <p class="descript">&#x25C0; Cookie Dough &#9658;</p>
-                </div>
-                <VueButton @click="addToCart" class="add-to-cart">
-                  Add to Cart
-                  <img class="cart-icon" src="../../../assets/img/shopping-cart.png" alt="Shopping cart icon.">
-                </VueButton>
-
-              </td>
-            </tr>
-          </table>
+          <CYOMenu/>
         </td>
 
       </tr>
@@ -85,17 +36,16 @@
 <script>
 import Background from "@/components/Background.vue";
 import VueButton from "@/components/Button.vue";
-
-import featured0 from '../../../assets/img/featured0.png';
-import featured1 from '../../../assets/img/featured1.png';
-import featured2 from '../../../assets/img/featured2.png';
-import featured3 from '../../../assets/img/featured3.png';
+import FeaturedMenu from "@/components/page-content/FeaturedMenu.vue";
+import CYOMenu from "@/components/page-content/CYOMenu.vue";
 
 export default {
   name: 'Menu',
   components: {
+    FeaturedMenu,
     Background,
     VueButton,
+    CYOMenu,
   },
   data() {
     return {
@@ -104,16 +54,6 @@ export default {
         {name: 'CYO cone', price: 499, qty: 1, details: {
             cone: 'waffle', scoops: ['chocolate', 'strawberry'], toppings: ['sprinkles']
           }}
-      ],
-      featured: [
-        [
-          {name: "S'mores", price: 123, image: featured0, css: 'featured-image f0'},
-          {name: 'Berry Blast', price: 123, image: featured1, css: 'featured-image f1'},
-        ],
-        [
-          {name: 'Strawberry Cheesecake', price: 123, image: featured2, css: 'featured-image f2'},
-          {name: 'Peanut Butter', price: 123, image: featured3, css: 'featured-image f3'},
-        ],
       ],
     }
   },
@@ -133,84 +73,10 @@ export default {
 
 <style scoped>
 
-.descript {
-  font-weight: normal;
-  text-align: center;
-}
-
-.add-to-cart {
-  position: absolute;
-  left: 60%;
-  top: 85%;
-}
-
-.featured-price {
-  margin: 0;
-  position: absolute;
-  top: 87%;
-  display: inline-block;
-}
-
-.this-button img {
-  width: 80%;
-}
-
-.this-button {
-  margin: 0;
-  display: inline-block;
-  width: 20%;
-  height: 18%;
-  border-radius: 15px;
-  padding: 5px 5px 5px 5px;
-  cursor: pointer;
-  background-color: rgb(115, 221, 67);
-  color: white;
-  transition: background-color 0.3s;
-  float: right;
-  position: absolute;
-  left: 75%;
-  top: 75%;
-}
-
-.this-button:hover {
-  background-color:  rgb(64, 126, 36);
-}
-
-.f0 {
-  width: 60%;
-}
-
-.f1 {
-  width: 60%;
-}
-
-.f2 {
-  width: 55%;
-}
-
-.f3 {
-  width: 65%;
-}
-
-.featured-image {
-  display: block;
-  margin: auto;
-}
-
 h3 {
   font-size: 12pt;
   text-align: center;
   margin-top: 2mm;
-}
-
-.small-center {
-  padding: 10px 10px 0 10px;
-  margin: 10px;
-  color: rgba(40, 40, 40);
-  font-weight: bold;
-  text-align: left;
-  background-color: rgba(244, 255, 256, .95);
-  position: relative;
 }
 
 .cart-icon {
