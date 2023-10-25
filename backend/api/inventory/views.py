34c5de9adc0c0cc0e4e62f0ff5de71042e2ecfdd
Jack Agren <jack.agren1@gmail.com>
2023-10-25 11:31:38 -0400

    working inv api. Need to figure out integration

--- backend/api/inventory/views.py
+++ backend/api/inventory/views.py
@@ -1,17 +1,18 @@
 # Create your views here.
 from django.http import JsonResponse, HttpResponse
 from rest_framework import viewsets, mixins, generics, status
-from rest_framework.decorators import api_view, APIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from .serializers import InventorySerializer
 from .models import Inventory
 
-# ./get_inv?description=<description>
-
+# /inventory/get_inventory
 class InventoryList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
-    
+
+# /inventory/get_inventory/<description>    
 class InventoryDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Inventory.objects.all()
     serializer_class = InventorySerializer
