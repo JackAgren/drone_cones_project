5d06e78a77d680d9037a71b20edb5bf7c78792e8
Jack Agren <jack.agren1@gmail.com>
2023-10-26 11:54:04 -0400

    added category to inventory

--- backend/api/inventory/models.py
+++ backend/api/inventory/models.py
@@ -4,4 +4,5 @@ class Inventory(models.Model):
     quantity = models.IntegerField()
     costPerUnit = models.IntegerField()
     dateFilled = models.DateField()
+    category = models.CharField(max_length=200)
     description = models.CharField(max_length=200, primary_key=True) #Unique identifier
