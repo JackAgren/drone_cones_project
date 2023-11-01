79345b13d65d7fc8d4750351904e6343e7dc5911
Boston Musgrave <a02267923@usu.edu>
2023-11-01 11:31:03 -0600

    Sales price field Added

--- backend/api/inventory/models.py
+++ backend/api/inventory/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Inventory(models.Model):
     quantity = models.IntegerField()
-    costPerUnit = models.IntegerField()
+    costPerUnit = models.FloatField()
     dateFilled = models.DateField()
     category = models.CharField(max_length=200)
     description = models.CharField(max_length=200, primary_key=True) #Unique identifier
+    salesPrice = models.FloatField()
