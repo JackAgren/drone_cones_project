504d12d91de43a6be25d611f975706d503f3b349
Soakeson <skyler.oakeson@gmail.com>
2023-11-04 13:00:14 -0600

    Added a default for salesPrice.

--- backend/api/inventory/models.py
+++ backend/api/inventory/models.py
@@ -6,4 +6,4 @@ class Inventory(models.Model):
     dateFilled = models.DateField()
     category = models.CharField(max_length=200)
     description = models.CharField(max_length=200, primary_key=True) #Unique identifier
-    salesPrice = models.FloatField()
+    salesPrice = models.FloatField(default=0)
