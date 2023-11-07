a39de850c32894563c62bedccf2ae729685dba6b
Soakeson <skyler.oakeson@gmail.com>
2023-11-07 15:57:34 -0700

    Added a better way to alter inventory items; Deprecated increment and deprecated in favor or update_item.

--- backend/api/inventory/urls.py
+++ backend/api/inventory/urls.py
@@ -5,8 +5,7 @@ from .import views
 app_name = 'inventory'
 urlpatterns = [
     path('inventory_search', views.inventory_search, name='list'),
-    path('increment', views.increment_inventory, name='increment_inventory'),
-    path('decrement', views.decrement_inventory, name='decrement_inventory'),
+    path('update_item', views.update_item, name='update_item'),
     path('add', views.add_inventory, name='add_inventory'),
     path('remove', views.remove_inventory, name='remove_inventory'),
 ]
