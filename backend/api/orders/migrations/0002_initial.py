2fdf41ed37f2bc66921c7b17db350db7bf6987d6
Jack Agren <jack.agren1@gmail.com>
2023-11-10 10:10:24 -0700

    Drone Functions

--- backend/api/orders/migrations/0002_initial.py
+++ backend/api/orders/migrations/0002_initial.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        ('orders', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('drone_operator', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='droneinfo',
-            name='ownerID',
+            model_name='orders',
+            name='userID',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL),
         ),
     ]
